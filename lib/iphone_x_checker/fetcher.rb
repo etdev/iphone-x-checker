d56805cb1fe2d541e66aed51737ce67db39ede78
Eric Turner <ericturnerdev@gmail.com>
2017-11-26 23:08:03 +0900

    Change zip code

--- lib/iphone_x_checker/fetcher.rb
+++ lib/iphone_x_checker/fetcher.rb
@@ -2,7 +2,7 @@ require "rest-client"
 
 module IphoneXChecker
   class Fetcher
-    URL = "https://www.apple.com/jp/shop/retail/pickup-message?pl=true&parts.0=MQAY2J/A&location=106-0031".freeze
+    URL = "https://www.apple.com/jp/shop/retail/pickup-message?pl=true&parts.0=MQAY2J/A&location=105-0001".freeze
 
     def fetch_data
       RestClient.get(URL)
